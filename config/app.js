f7b9df9d23b51a7600fe7c37b9099ab76616194e
Gani Georgiev <gani.georgiev@gmail.com>
2018-03-08 15:28:46 +0200

    rearrange config settings

--- config/app.js
+++ config/app.js
@@ -1,8 +1,8 @@
 module.exports = {
-    authCookieKey: 'AUTH_DATA',
     apiUrl:        'http://localhost:8090/api',
     supportEmail:  'support@gofreta.com',
     baseTitle:     'Gofreta Admin',
     homepageRoute: 'collection-index',
     dateFormat:    'YYYY-MM-DD HH:mm:ss'
+    authCookieKey: 'AUTH_DATA',
 }
